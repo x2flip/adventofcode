9abd2dca1a59f1efc20205358cde7bc5a1d466b7
x2flip <gianni.m.baudino@gmail.com>
2023-12-05 19:55:19 -0800

    initial

---
+++ 2023/day4/target/debug/deps/day4-c71ddbec1d46af14.d
@@ -0,0 +1,5 @@
+/home/gbaudino/projects/adventofcode/2023/day4/target/debug/deps/libday4-c71ddbec1d46af14.rmeta: src/main.rs
+
+/home/gbaudino/projects/adventofcode/2023/day4/target/debug/deps/day4-c71ddbec1d46af14.d: src/main.rs
+
+src/main.rs:
---
+++ 2023/day4/target/debug/deps/day4-ef585482e9a0a9bf.d
@@ -0,0 +1,5 @@
+/home/gbaudino/projects/adventofcode/2023/day4/target/debug/deps/libday4-ef585482e9a0a9bf.rmeta: src/main.rs
+
+/home/gbaudino/projects/adventofcode/2023/day4/target/debug/deps/day4-ef585482e9a0a9bf.d: src/main.rs
+
+src/main.rs:
